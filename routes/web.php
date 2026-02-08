07b216eb321b8340d412b26539153aaf3a830361
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-02-08 17:24:09 +0100

    final test ci-cd

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,18 @@ use App\Events\UserRegistered;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     $user = User::find(1);
+//     event(new UserRegistered($user));
+// });
 Route::get('/', function () {
-    $user = User::find(1);
-    event(new UserRegistered($user));
+    return response()->json([
+        'message' => 'Ebook Authentication Service is running.',
+        // 'api routes' => [
+        //     '/notifications' => 'Get all notifications',
+        //     '/notifications/{id}/read' => 'Mark a notification as read',
+        //     '/notifications/read-all' => 'Mark all notifications as read',
+        // ],
+        'pipeline' => 'CI/CD pipeline is set up for automated deployments.',
+    ]);
 });
