6db7a6c3447f169d7ff6200967d800c0f8525fab
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-02-08 17:02:04 +0100

    add github actions deploy workflow

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+set -e
+
+cd /var/www/ebook-authentication-service
+
+git pull origin main
+
+composer install --no-dev --optimize-autoloader
+
+php artisan migrate --force
+
+php artisan optimize:
+php artisan config:cache
+php artisan route:cache
+
+sudo systemctl reload php8.3-fpm
+
+supervisorctl reread
+supervisorctl update
+supervisorctl restart ebook-notifications-queue
+supervisorctl restart ebook-notifications-listener
