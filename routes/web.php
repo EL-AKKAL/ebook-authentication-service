0fe8e1c86419deb1b45528dfa0a0f97a9b132b7d
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-02-13 15:05:48 +0100

    clear comments in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
-
-use App\Events\UserRegistered;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     $user = User::find(1);
-//     event(new UserRegistered($user));
-// });
 Route::get('/', function () {
     return response()->json([
         'message' => 'Ebook Authentication Service is running.',
