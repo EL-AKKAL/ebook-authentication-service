07a91a884fd8c6e42f4d3a05d9a7647f34467841
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2026-02-15 19:39:37 +0000

    ...

---
+++ app/Http/Controllers/NewPasswordController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Auth\Events\PasswordReset;
+
+class NewPasswordController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+         $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                    'remember_token' => Str::random(60),
+                ])->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+         return $status === Password::PASSWORD_RESET
+            ? response()->json(['status' => __($status)])
+            : response()->json(['email' => __($status)], 422);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NewPasswordController;
 use App\Http\Controllers\PasswordResetLinkController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,9 @@ Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('forgot-password', PasswordResetLinkController::class)
     ->name('password.request');
 
+Route::post('reset-password', NewPasswordController::class)
+    ->name('password.update');
+
 Route::middleware('auth:api')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout'])
@@ -24,3 +28,4 @@ Route::middleware('auth:api')->group(function () {
 });
 
 
+  
